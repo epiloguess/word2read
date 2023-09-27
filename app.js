3c6348dab9c79cd0829725b202849c80e6bee356
epiloguess <hellowyho@gmail.com>
2023-09-27 18:03:40 +0800

    v0.0.8

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import multer from "multer";
 import { processEpub } from "./src/epub-process.js";
-// import { processEpub } from "./src/epub-process1.js";
 import cors from "cors";
 
 const app = express();
@@ -28,6 +27,7 @@ const upload = multer({ storage });
 app.use(cors(corsOptions));
 
 app.post("/upload", upload.single("file"), async (req, res) => {
+  console.log("new req...");
   let start = Date.now();
   let result = await processEpub(req.file.path);
   let end = Date.now();
@@ -35,15 +35,15 @@ app.post("/upload", upload.single("file"), async (req, res) => {
   res.json(result);
 });
 
-let sum = 0;
-for (let index = 0; index < 20; index++) {
-  let start = Date.now();
-  await processEpub("./public/book.epub");
-  let end = Date.now();
-  console.log(`The loop took ${end - start} ms`);
-  sum += end - start;
-}
-console.log("average:", sum / 20);
+// let sum = 0;
+// for (let index = 0; index < 20; index++) {
+//   let start = Date.now();
+// await processEpub("./public/book.epub");
+//   let end = Date.now();
+//   console.log(`The loop took ${end - start} ms`);
+//   sum += end - start;
+// }
+// console.log("average:", sum / 20);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- src/epub-process.js
+++ src/epub-process.js
@@ -8,50 +8,41 @@ function getCocaRank(word) {
   }
   return 0;
 }
-export async function processEpub(filePath, level = 5000) {
-  const epubObj = await parseEpub(filePath, {
-    type: "path",
-  });
 
-  const stemedMap = new Map();
-  let rankCount;
-  for (const section of epubObj.sections) {
-    const mdcontent = section
-      .toMarkdown()
-      .replace(/[()#\[*]/g, "")
-      .replace(/\]/g, ".");
-    const allSentencesExtend = nlp(mdcontent).compute("root").json();
+function contentRegex(content) {
+  return content.replace(/[()#\[*]/g, "").replace(/\]/g, ".");
+}
 
-    for (const sentence of allSentencesExtend) {
-      rankCount = sentence.terms.reduce(
-        (sum, term) => sum + getCocaRank(term.root || term.normal),
-        0
-      );
-      for (const term of sentence.terms) {
-        let stemed = term.root || term.normal;
-        let rank = getCocaRank(stemed);
-        if (stemed.length > 2 && rank > level) {
-          if (!stemedMap.has(stemed)) {
-            stemedMap.set(stemed, {
-              stemed,
-              tokens: new Set(),
-              examples: new Map(),
-              count: 0,
-              rank,
-            });
-          }
-          const obj = stemedMap.get(stemed);
-          obj.tokens.add(term.text);
-          obj.count++;
-          if (sentence.terms.length > 5) {
-            obj.examples.set(sentence.text, rankCount);
-          }
-        }
+function processSentence(sentence, stemedMap, level) {
+  let rankCount = sentence.terms.reduce(
+    (sum, term) => sum + getCocaRank(term.root || term.normal),
+    0
+  );
+  for (const term of sentence.terms) {
+    let stemed = term.root || term.normal;
+    let rank = getCocaRank(stemed);
+    if (stemed.length > 2 && rank > level) {
+      if (!stemedMap.has(stemed)) {
+        stemedMap.set(stemed, {
+          stemed,
+          tokens: new Set(),
+          examples: new Map(),
+          count: 0,
+          rank,
+        });
+      }
+      const obj = stemedMap.get(stemed);
+      obj.tokens.add(term.text);
+      obj.count++;
+      if (sentence.terms.length > 5) {
+        obj.examples.set(sentence.text, rankCount);
       }
     }
   }
+}
 
-  const objectArray = Array.from(stemedMap.values(), (obj) => {
+function stemedMapToObjArray(stemedMap) {
+  let objectArray = Array.from(stemedMap.values(), (obj) => {
     obj.tokens = [...obj.tokens];
     obj.examples = Array.from(obj.examples, ([example, rankCount]) => ({
       example,
@@ -60,4 +51,19 @@ export async function processEpub(filePath, level = 5000) {
     return obj;
   }).sort((a, b) => a.count - b.count);
   return objectArray;
+}
+export async function processEpub(filePath, level = 5000) {
+  const epubObj = await parseEpub(filePath, {
+    type: "path",
+  });
+
+  const stemedMap = new Map();
+  for (const section of epubObj.sections) {
+    const content = contentRegex(section.toMarkdown());
+    const allSentencesExtend = nlp(content).compute("root").json();
+    for (const sentence of allSentencesExtend) {
+      processSentence(sentence, stemedMap, level);
+    }
+  }
+  return stemedMapToObjArray(stemedMap);
 }
