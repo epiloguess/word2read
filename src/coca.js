72ff00ea09b1cfe78e1415f8d9345c07cfc3185f
epiloguess <hellowyho@gmail.com>
2023-10-12 15:37:26 +0800

    20231012

--- src/coca.js
+++
@@ -1,6 +0,0 @@
-import fs from "node:fs";
-const readFileLines = (filename) => fs.readFileSync(filename).toString("utf8").split("\r\n");
-export let cocaMap = new Map();
-[...new Set(readFileLines("./public/coca.txt"))].forEach((value, index) => {
-    cocaMap.set(value, { index });
-});
